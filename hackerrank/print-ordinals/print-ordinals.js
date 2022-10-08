db6e1823dd928af58de24ef8c2d5db1b37264bc3
Andrea Vanelli <andrea.vanelli@gmail.com>
2022-10-08 16:17:06 +0200

    dummy algo solver

---
+++ hackerrank/print-ordinals/print-ordinals.js
@@ -0,0 +1,20 @@
+
+/**
+ *
+ * @param {string} testData
+ */
+const main = (testData) => {
+  // parse input
+  return solve()
+}
+
+/**
+ *
+ * @param {array} testData
+ */
+const solve = (numbers) => {
+  // solution
+  return 'test'
+}
+
+module.exports = { main }
